2ea23e72c4c02f71874de0a381d35a8f3b4d868d
Su Khai Koh <sukhai.koh@manapitch.com>
2019-07-25 00:04:53 -0700

    Add testhelper project.

---
+++ testhelper/src/main/java/com/sukhaikoh/roctopus/testhelper/SchedulersTestExtension.kt
@@ -0,0 +1,26 @@
+package com.sukhaikoh.roctopus.testhelper
+
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+/**
+ * A Junit 5 `Extension` for a test method or test class that helps
+ * initializing [Schedulers] for each unit test.
+ *
+ * This class implements both [BeforeEachCallback] and [AfterEachCallback] to
+ * initialize and tear down each unit test.
+ */
+class SchedulersTestExtension : BeforeEachCallback, AfterEachCallback {
+
+    override fun afterEach(context: ExtensionContext?) {
+        RxJavaPlugins.reset()
+    }
+
+    override fun beforeEach(context: ExtensionContext?) {
+        RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+    }
+}
